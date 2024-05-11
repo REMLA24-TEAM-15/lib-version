3aca98dec6779a1cadbcd1c650d3fd85dd7b7323
Marianna Louizidou <M.Louizidou@student.tudelft.nl>
2024-05-11 18:15:22 +0200

    release debug

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 import os
 
 version = {}
-with open(os.path.join("src", "version.py")) as fp:
+with open(os.path.join("lib-version", "version.py")) as fp:
     exec(fp.read(), version)
 
 setup(
-    name='lib-version',
+    name='lib-version_URLPhishing',
     version=version['__version__'],
     packages=find_packages(),
     description='A library to manage and track versions of software components'

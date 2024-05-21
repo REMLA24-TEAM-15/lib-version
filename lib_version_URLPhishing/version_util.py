7569256f4439040ca128f1e5897f7d837755b9b2
Marianna Louizidou <M.Louizidou@student.tudelft.nl>
2024-05-21 16:46:20 +0200

    change back to flake8

--- lib_version_URLPhishing/version_util.py
+++ lib_version_URLPhishing/version_util.py
@@ -1,19 +1,18 @@
-"""
-Module for version utility functions.
-"""
-
 from .version import __version__
 
 
 class VersionUtil:
     """
-    A utility class for version-related operations.
+    A utility class to get the version of the library.
     """
 
     @staticmethod
     def get_version():
         """
         Get the current version of the library.
+
+        Returns:
+            str: The current version of the library.
         """
         return __version__
 
